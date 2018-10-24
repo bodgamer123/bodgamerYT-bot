4ba1f11ca4a56731b51be38ad8b97d389b9e82eb
bodgamer123 <44031670+bodgamer123@users.noreply.github.com>
2018-10-24 11:49:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -280,88 +280,9 @@ client.on('message', function(msg) {
 
 
   
-client.on("message", (message) => {
-    /// ALPHA CODES
-   if (message.content.startsWith("$new")) {   
-        const reason = message.content.split(" ").slice(1).join(" ");   
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-    }
- 
-  
-  
- 
-  if (message.content.startsWith("$close")) {
-        if (!message.channel.name.startsWith(`$ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-       message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`$confirm\`. This will time out in 10 seconds and be cancelled.`)
-           .then((m) => {
-               message.channel.awaitMessages(response => response.content === '$confirm', {
-                       max: 1,
-                       time: 10000,
-                       errors: ['time'],
-                   })    
-                   .then((collected) => {
-                       message.channel.delete();
-                   })    /// ALPHA CODES
-                   .catch(() => {
-                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                           m2.delete();
-                       }, 3000);
-                   });
-           });
-   }
- 
-});
-   /// ALPHA CODES
-
 client.on('message', message =>{
   if(message.content.startsWith('$join')){
     const voiceChannel = message.member.voiceChannel
     voiceChannel.join();
     message.channel.send("تم الأتصال بالروم الصوتي")
 }})
-
-
-
-client.on('message', message =>{
-  if(message.content.startsWith('$leave')){
-    const voiceChannel = message.member.voiceChannel
-    voiceChannel.disconnect();
-    message.channel.send("تم الخروج من الروم الصوتي")
-}})
-
-
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-  
-  
